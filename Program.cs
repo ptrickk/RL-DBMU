6429c37fe8324ac85a425814674237e4a3df00f6
SameplayerDE <asameplayer@gmail.com>
2020-12-02 17:31:12 +0100

    IMPROVEMENT !!!!!!!!

--- Program.cs
+++ Program.cs
@@ -13,19 +13,27 @@ namespace RL_DMBU
 
         static void Main(string[] args)
         {
-            List<Messung> messungen = new List<Messung>();
+
+            PlayerList playerList = new PlayerList();
+            MeasurementList measurementList = new MeasurementList();
             // Change the username, password and database according to your needs
             // You can ignore the database option if you want to access all of them.
             // 127.0.0.1 stands for localhost and the default port to connect.
             string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=rocketleague;";
             // Your query,
             //string query = "SELECT Spiele, Siege, Tore, Torschüsse, Vorlagen, Paraden, Hereingaben, Befreiungsschläge, Retter, Zerstörungen, 3v3, 2v2, 1v1 FROM messung WHERE SpielerID = 1 ORDER BY Datum DESC";
-            string query = "SELECT * FROM messung Order by Datum ASC";
+            string playerQuery = "SELECT * FROM spieler";
+            string measurementQuery = "SELECT * FROM messung Order by Datum ASC";
 
             // Prepare the connection
             MySqlConnection databaseConnection = new MySqlConnection(connectionString);
-            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
-            commandDatabase.CommandTimeout = 60;
+
+            MySqlCommand playerCommand = new MySqlCommand(playerQuery, databaseConnection);
+            MySqlCommand measurementCommand = new MySqlCommand(measurementQuery, databaseConnection);
+
+            playerCommand.CommandTimeout = 60;
+            measurementCommand.CommandTimeout = 60;
+
             MySqlDataReader reader;
 
             // Let's do it !
@@ -33,45 +41,47 @@ namespace RL_DMBU
             {
                 // Open the database
                 databaseConnection.Open();
+                // Execute the player query
+                reader = playerCommand.ExecuteReader();
 
-                // Execute the query
-                reader = commandDatabase.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        //create player with data from query
+                        Player player = new Player(reader.GetInt32("SpielerID"), reader.GetString("Name"), reader.GetString("Spielername"));
 
-                // All succesfully executed, now do something
+                        playerList.Add(player);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No Players found.");
+                }
 
-                // IMPORTANT : 
-                // If your query returns result, use the following processor :
+                reader.Close();
 
-                
+                //prepare measurement command
+                reader = measurementCommand.ExecuteReader();
 
                 if (reader.HasRows)
                 {
                     while (reader.Read())
                     {
 
-                        Messung m = new Messung(
-                            reader.GetInt32(0),
-                            reader.GetDateTime(1),
-                            reader.GetInt32(2),
-                            reader.GetInt32(3),
-                            reader.GetInt32(4),
-                            reader.GetInt32(5),
-                            reader.GetInt32(6),
-                            reader.GetInt32(7),
-                            reader.GetInt32(8),
-                            reader.GetInt32(9),
-                            reader.GetInt32(10),
-                            reader.GetInt32(11),
-                            reader.GetInt32(12),
-                            reader.GetInt32(13),
-                            reader.GetInt32(14),
-                            reader.GetInt32(15)
+                        Measurement m = new Measurement(
+                            reader.GetInt32("MessungsID"),
+                            reader.GetDateTime("Datum"),
+                            reader.GetInt32("SpielerID")
                             );
 
-                        messungen.Add(m);
-                    }
+                        for (int i = 3; i < reader.FieldCount; i++)
+                        {
+                            m.Add(reader.GetName(i), reader.GetInt32(reader.GetName(i)));
+                        }
 
-                    
+                        measurementList.Add(m);
+                    }
 
                 }
                 else
@@ -87,28 +97,17 @@ namespace RL_DMBU
                 // Show any error message.
                 Console.WriteLine(ex.ToString());
             }
-            Console.WriteLine("Alle Daten wurden eingelesen!");
+
+            Console.WriteLine("Alle Daten wurden eingelesen.");
             Console.ReadLine();
 
-            /*
-             * Menü
-             * 1: Hinzufügen
-             * 2: Bearbeiten
-             * 3: Tabelle ausgeben
-             * 
-             */
+            playerList.PrintAll();
 
-            int vorher = 0;
-            int current = 0;
-            int spielerID = 2;
-            foreach (KeyValuePair<DateTime, int> s in Messung.getIntValueWithDate(Messung.getMessungenBySID(spielerID, messungen), "Spiele"))
-            {
-                current = s.Value;
-                Console.WriteLine(current + " Differenz von: +" + (current - vorher) + " Datum: " + s.Key.ToShortDateString());
-                vorher = current;
-            }
+            Console.WriteLine("Press any key to print measurements.");
+            Console.ReadLine();
 
-            
+            measurementList.PrintAll();
+
             Console.ReadLine();
 
         }
