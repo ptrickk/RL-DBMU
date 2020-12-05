3f69a7476ad8c1bc5ee64c0d77ba7bc5ef4fe68a
SameplayerDE <asameplayer@gmail.com>
2020-12-05 14:23:28 +0100

    Added Chart Types

--- ChartForm.cs
+++ ChartForm.cs
@@ -17,6 +17,8 @@ namespace RL_DBMU
         private System.ComponentModel.IContainer components;
         private Button button1;
         private CheckBox showValues;
+        private TrackBar trackBar1;
+        private ComboBox comboBox1;
         private int _count = 0;
 
         public ChartForm(MeasurementList measurementList)
@@ -27,8 +29,8 @@ namespace RL_DBMU
 
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             this._chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.fields = new System.Windows.Forms.CheckedListBox();
             this.diff = new System.Windows.Forms.CheckBox();
@@ -36,22 +38,25 @@ namespace RL_DBMU
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
             this.showValues = new System.Windows.Forms.CheckBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this._chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // _chart
             // 
-            chartArea2.Name = "ChartArea1";
-            this._chart.ChartAreas.Add(chartArea2);
+            chartArea4.Name = "ChartArea1";
+            this._chart.ChartAreas.Add(chartArea4);
             this._chart.Cursor = System.Windows.Forms.Cursors.Default;
             this._chart.DataSource = this._chart.Images;
             this._chart.ImeMode = System.Windows.Forms.ImeMode.Off;
-            legend2.Name = "Legend1";
-            this._chart.Legends.Add(legend2);
+            legend4.Name = "Legend1";
+            this._chart.Legends.Add(legend4);
             this._chart.Location = new System.Drawing.Point(148, 57);
             this._chart.Name = "_chart";
             this._chart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
-            this._chart.Size = new System.Drawing.Size(696, 300);
+            this._chart.Size = new System.Drawing.Size(768, 358);
             this._chart.SuppressExceptions = true;
             this._chart.TabIndex = 0;
             this._chart.TabStop = false;
@@ -80,6 +85,7 @@ namespace RL_DBMU
             this.fields.Name = "fields";
             this.fields.Size = new System.Drawing.Size(120, 199);
             this.fields.TabIndex = 1;
+            this.fields.SelectedIndexChanged += new System.EventHandler(this.fields_SelectedIndexChanged);
             // 
             // diff
             // 
@@ -111,7 +117,7 @@ namespace RL_DBMU
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 334);
+            this.button1.Location = new System.Drawing.Point(12, 392);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(120, 23);
             this.button1.TabIndex = 5;
@@ -128,10 +134,39 @@ namespace RL_DBMU
             this.showValues.TabIndex = 6;
             this.showValues.Text = "Zeige Werte";
             this.showValues.UseVisualStyleBackColor = true;
+            this.showValues.CheckedChanged += new System.EventHandler(this.showValues_CheckedChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 341);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(120, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Line",
+            "Spline",
+            "SplineArea",
+            "Area"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 310);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.Text = "Line";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // ChartForm
             // 
-            this.ClientSize = new System.Drawing.Size(928, 384);
+            this.ClientSize = new System.Drawing.Size(928, 427);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.showValues);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePicker2);
@@ -141,6 +176,7 @@ namespace RL_DBMU
             this.Controls.Add(this._chart);
             this.Name = "ChartForm";
             ((System.ComponentModel.ISupportInitialize)(this._chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -185,7 +221,7 @@ namespace RL_DBMU
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            generate();
         }
 
         private void toolTip1_Popup(object sender, PopupEventArgs e)
@@ -195,6 +231,24 @@ namespace RL_DBMU
 
         private void button1_Click(object sender, EventArgs e)
         {
+            generate();
+        }
+
+        private void generate()
+        {
+            SeriesChartType type = SeriesChartType.Line;
+            if (comboBox1.Text == "Spline")
+            {
+                type = SeriesChartType.Spline;
+            }
+            else if (comboBox1.Text == "SplineArea")
+            {
+                type = SeriesChartType.SplineArea;
+            }
+            else if (comboBox1.Text == "Area")
+            {
+                type = SeriesChartType.Area;
+            }
             if (diff.Checked)
             {
                 _count = 0;
@@ -202,8 +256,8 @@ namespace RL_DBMU
                 foreach (string field in fields.CheckedItems)
                 {
                     Series series = new Series(field);
-                    series.ChartType = SeriesChartType.Line;
-                    series.BorderWidth = 5;
+                    series.ChartType = type;
+                    series.BorderWidth = trackBar1.Value;
                     series.IsValueShownAsLabel = showValues.Checked;
 
                     _chart.Series.Add(series);
@@ -240,8 +294,8 @@ namespace RL_DBMU
                 foreach (string field in fields.CheckedItems)
                 {
                     Series series = new Series(field);
-                    series.ChartType = SeriesChartType.Line;
-                    series.BorderWidth = 5;
+                    series.ChartType = type;
+                    series.BorderWidth = trackBar1.Value;
                     series.IsValueShownAsLabel = showValues.Checked;
 
                     _chart.Series.Add(series);
@@ -256,13 +310,32 @@ namespace RL_DBMU
                             }
                         }
                     }
-                    foreach(DataPoint point in series.Points)
+                    foreach (DataPoint point in series.Points)
                     {
                         point.ToolTip = point.YValues[0] + "";
                     }
                     _count++;
                 }
             }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            generate();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            generate();
+        }
+
+        private void fields_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void showValues_CheckedChanged(object sender, EventArgs e)
+        {
+            generate();
         }
     }
 }
